d7a18f763f3144792accdc5d3e3166e3a156895a
Tanya Libatter <tanya.libatter@kaltura.com>
2018-01-09 11:37:01 +0200

    feat: init ott analytics plugin (#1)

---
+++ src/ott-analytics.js
@@ -0,0 +1,272 @@
+//@flow
+import {BasePlugin} from 'playkit-js'
+import {OTTBookmarkService, RequestBuilder} from 'playkit-js-providers/dist/playkit-bookmark-service'
+
+type OttAnalyticsEventType = { [event: string]: string };
+const MEDIA_TYPE = 'MEDIA';
+const CONCURRENT = 'Concurrent';
+const OttAnalyticsEvent: OttAnalyticsEventType = {
+  LOAD: 'LOAD',
+  PLAY: 'PLAY',
+  PAUSE: 'PAUSE',
+  FINISH: 'FINISH',
+  FIRST_PLAY: 'FIRST_PLAY',
+  BITRATE_CHANGE: 'BITRATE_CHANGE',
+  HIT: 'HIT'
+};
+
+export default class OttAnalytics extends BasePlugin {
+  /**
+   * The default configuration of the plugin.
+   * @type {Object}
+   * @static
+   */
+  static defaultConfig: Object = {
+    serviceUrl: 'http://api-preprod.ott.kaltura.com/v4_6/api_v3',
+    mediaHitInterval: 30,
+    startTime: null
+  };
+
+  /**
+   * Whether the ima plugin is valid.
+   * @static
+   * @override
+   * @public
+   */
+  static isValid(): boolean {
+    return true;
+  }
+
+  _isPlaying: boolean;
+  _concurrentFlag: boolean;
+  _fileId: number;
+  _didFirstPlay: boolean;
+  _mediaHitInterval: number;
+  _continueTime: number;
+  _playFromContinue: boolean;
+  _isPlaying: boolean;
+
+  /**
+   * @constructor
+   * @param {string} name - The plugin name.
+   * @param {Player} player - The player instance.
+   * @param {Object} config - The plugin config.
+   */
+  constructor(name: string, player: Player, config: Object) {
+    super(name, player, config);
+    this._initializeMembers();
+    this._registerListeners();
+    this._sendAnalytics(OttAnalyticsEvent.LOAD, this._eventParams);
+  }
+
+  /**
+   * Destroys the plugin.
+   * @override
+   * @public
+   * @returns {void}
+   */
+  destroy(): void {
+    this.eventManager.destroy();
+  }
+
+  /**
+   * Registers the player listeners.
+   * @private
+   * @returns {void}
+   */
+  _registerListeners(): void {
+    const PlayerEvent = this.player.Event;
+    this.eventManager.listen(this.player, PlayerEvent.FIRST_PLAY, () => this._onFirstPlay());
+    this.eventManager.listen(this.player, PlayerEvent.PLAY, () => this._onPlay());
+    this.eventManager.listen(this.player, PlayerEvent.PAUSE, () => this._onPause());
+    this.eventManager.listen(this.player, PlayerEvent.ENDED, () => this._onEnded());
+    this.eventManager.listen(this.player, PlayerEvent.SEEKED, () => this._onSeeked());
+    this.eventManager.listen(this.player, PlayerEvent.VIDEO_TRACK_CHANGED, () => this._onVideoTrackChanged());
+    this.eventManager.listen(this.player, PlayerEvent.CHANGE_SOURCE_STARTED, () => this._onChangeSourceStarted());
+    this.eventManager.listen(this.player, PlayerEvent.SOURCE_SELECTED, event => this._onSourceSelected(event));
+  }
+
+  /**
+   * The source selected event listener.
+   * @param {Object} event - The event object.
+   * @private
+   * @returns {void}
+   */
+  _onSourceSelected(event: Object): void {
+    try {
+      const source = event.payload.selectedSource[0];
+      const parts = source.id.split(',');
+      const id = parts[0];
+      this._fileId = parseInt(id);
+    } catch (e) {
+      this.logger.error(e);
+    }
+  }
+
+  /**
+   * The play event listener.
+   * @private
+   * @returns {void}
+   */
+  _onPlay(): void {
+    this._isPlaying = true;
+    this._startMediaHitInterval();
+    this._sendAnalytics(OttAnalyticsEvent.PLAY, this._eventParams);
+  }
+
+  /**
+   * The pause event listener.
+   * @private
+   * @returns {void}
+   */
+  _onPause(): void {
+    this._isPlaying = false;
+    if (this._didFirstPlay) {
+      this._sendAnalytics(OttAnalyticsEvent.PAUSE, this._eventParams);
+    }
+  }
+
+  /**
+   * The ended event listener.
+   * @private
+   * @returns {void}
+   */
+  _onEnded(): void {
+    this._isPlaying = false;
+    this._clearMediaHitInterval();
+    this._sendAnalytics(OttAnalyticsEvent.FINISH, this._eventParams);
+  }
+
+  /**
+   * The first play event listener.
+   * @private
+   * @returns {void}
+   */
+  _onFirstPlay(): void {
+    this._didFirstPlay = true;
+    this._sendAnalytics(OttAnalyticsEvent.FIRST_PLAY, this._eventParams);
+  }
+
+  /**
+   * Sets _playFromContinue to false on seeked event.
+   * @private
+   * @returns {void}
+   */
+  _onSeeked(): void {
+    this._playFromContinue = false;
+  }
+
+  /**
+   * Send BITRATE_CHANGE analytic.
+   * @private
+   * @returns {void}
+   */
+  _onVideoTrackChanged(): void {
+    this._sendAnalytics(OttAnalyticsEvent.BITRATE_CHANGE, this._eventParams);
+  }
+
+  /**
+   * Sets _didFirstPlay to false on media change.
+   * @private
+   * @returns {void}
+   */
+  _onChangeSourceStarted(): void {
+    this._didFirstPlay = false;
+  }
+
+  /**
+   * Get the player params which relevant to analytics request.
+   * @private
+   * @returns {Object} - The player params
+   */
+  get _eventParams(): Object {
+    return {
+      mediaType: MEDIA_TYPE,
+      fileId: this._fileId,
+      mediaId: this.config.entryId,
+      position: this.player.currentTime
+    };
+  }
+
+  /**
+   * Send analytics.
+   * @param {string} action - The action
+   * @param {string} params - The params
+   * @private
+   * @returns {void}
+   */
+  _sendAnalytics(action: string, params: Object): void {
+    const playerData: Object = {
+      action: action,
+      averageBitrate: 0, totalBitrate: 0,
+      currentBitrate: 0, fileId: params.fileId,
+    };
+    const bookMark: Object = {
+      type: params.mediaType,
+      id: params.mediaId,
+      position: params.position,
+      playerData: playerData
+    };
+    const request: RequestBuilder = OTTBookmarkService.add(this.config.serviceUrl, this.config.ks, bookMark);
+    request.doHttpRequest()
+      .then((data) => {
+        if (data === CONCURRENT) {
+          this._concurrentFlag = true;
+        } else {
+          this.logger.debug('Analytics event sent', bookMark);
+        }
+      }, err => {
+        this.logger.error('Failed to send analytics event', bookMark, err);
+      });
+  }
+
+  /**
+   * Starts the media hit interval.
+   * @private
+   * @returns {void}
+   */
+  _startMediaHitInterval(): void {
+    this._clearMediaHitInterval();
+    this._mediaHitInterval = setInterval(() => {
+      if (this._isPlaying) {
+        this._playFromContinue = false;
+        if (this._concurrentFlag || this._eventParams.position === 0) {
+          return;
+        } else {
+          this._sendAnalytics(OttAnalyticsEvent.HIT, this._eventParams);
+        }
+      }
+    }, this.config.mediaHitInterval * 1000);
+  }
+
+  /**
+   * Clears the media hit interval.
+   * @private
+   * @returns {void}
+   */
+  _clearMediaHitInterval(): void {
+    clearInterval(this._mediaHitInterval);
+    this._mediaHitInterval = 0;
+  }
+
+  /**
+   * Initializes the class members.
+   * @private
+   * @returns {void}
+   */
+  _initializeMembers(): void {
+    this._isPlaying = false;
+    this._concurrentFlag = false;
+    this._fileId = 0;
+    this._didFirstPlay = false;
+    this._mediaHitInterval = 0;
+    this._isPlaying = false;
+    if (this.config.startTime) {
+      this._continueTime = this.config.startTime;
+      this._playFromContinue = true;
+    } else {
+      this._continueTime = 0;
+      this._playFromContinue = false;
+    }
+  }
+}
---
+++ test/src/ott-analytics.spec.js
@@ -0,0 +1,138 @@
+import '../../src/index'
+import {loadPlayer} from 'playkit-js'
+import * as TestUtils from 'playkit-js/test/src/utils/test-utils'
+
+describe('OttAnalyticsPlugin', function () {
+  let player, sandbox, sendSpy, config;
+
+  /**
+   * @param {string} ks - ks
+   * @param {Object} bookmark - event
+   * @return {void}
+   */
+  function verifyPayloadProperties(ks, bookmark) {
+    ks.should.equal(player.config.session.ks);
+    bookmark.id.should.equal(player.config.id);
+    if (bookmark.duration) {
+      bookmark.duration.should.equal(player.duration);
+    }
+  }
+
+  before(function () {
+    config = {
+      "id": 258457,
+      "name": "Big Hero 6",
+      "session": {
+        "partnerId": 198,
+        "ks": "djJ8MTk4fPIz_ugsVrW8JwEX7detBwnuNZq2YVowN9VlB1d8gkHLY1wR6-GaeGYBxD6XBQ6SvDw6crDHhFpvsi7jcudRS2t1bSNFgIT5H2sZrHAGg_uasYXV6YHsm43_d_PsKgmnunAjFniOYXggUo8cT9RtSPo="
+      },
+      "sources": {
+        "progressive": [{
+          "id": "391837,url",
+          "url": "http://api-preprod.ott.kaltura.com/v4_6/api_v3/service/assetFile/action/playManifest/partnerId/198/assetId/258457/assetType/media/assetFileId/391837/contextType/TRAILER/a.mp4",
+          "mimetype": "video/mp4"
+        }],
+        "dash": [],
+        "hls": [{
+          "id": "397008,applehttp",
+          "url": "http://api-preprod.ott.kaltura.com/v4_6/api_v3/service/assetFile/action/playManifest/partnerId/198/assetId/258457/assetType/media/assetFileId/397008/contextType/TRAILER/a.m3u8",
+          "mimetype": "application/x-mpegURL"
+        }]
+      },
+      "duration": 1000,
+      "type": "Unknown",
+      "dvr": false,
+      "metadata": {
+        "0": {"key": "Genre", "value": "Comedy|Action|Adventure|Animation|Family|Editor|"},
+        "1": {"key": "Parental Rating", "value": "G|R|"},
+        "2": {"key": "QUALITY", "value": "hd|sd|"},
+        "3": {"key": "Free", "value": "yes|no|adi|"},
+        "4": {"key": "Source", "value": "Web3|Editor|"},
+        "5": {"key": "Country", "value": ""},
+        "6": {"key": "QUALITY", "value": ""},
+        "7": {"key": "Epg ID", "value": ""},
+        "8": {"key": "Release year", "value": 2012},
+        "9": {"key": "Catchup allowed", "value": false},
+        "description": "*** Free *** The special bond that develops between plus-sized inflatable robot Baymax, and prodigy Hiro Hamada, who team up with a group of friends to form a band of high-tech heroes."
+      }
+    };
+    config.plugins = {
+      "ottAnalytics": {
+        "entryId": config.id,
+        "fileId": "392026",
+        "ks": config.session.ks,
+        "partnerId": config.session.partnerId,
+        "mediaHitInterval": 2
+      }
+    };
+  });
+
+  beforeEach(function () {
+    sandbox = sinon.sandbox.create();
+    sendSpy = sandbox.spy(XMLHttpRequest.prototype, 'send');
+    player = loadPlayer(config);
+  });
+
+  afterEach(function () {
+    sandbox.restore();
+    player.destroy();
+    TestUtils.removeVideoElementsFromTestPage();
+  });
+
+  it('should send widget loaded', () => {
+    const payload = JSON.parse(sendSpy.lastCall.args[0]);
+    verifyPayloadProperties(payload.ks, payload.bookmark);
+    payload.bookmark.playerData.action.should.equal("LOAD");
+  });
+
+  it('should send first play', (done) => {
+    player.addEventListener(player.Event.FIRST_PLAY, () => {
+      const payload = JSON.parse(sendSpy.lastCall.args[0]);
+      verifyPayloadProperties(payload.ks, payload.bookmark);
+      payload.bookmark.playerData.action.should.equal("FIRST_PLAY");
+      done();
+    });
+    player.play();
+  });
+
+  it('should send pause', (done) => {
+    player.addEventListener(player.Event.PAUSE, () => {
+      const payload = JSON.parse(sendSpy.lastCall.args[0]);
+      verifyPayloadProperties(payload.ks, payload.bookmark);
+      payload.bookmark.playerData.action.should.equal("PAUSE");
+      done();
+    });
+    player.play();
+    setTimeout(function () {
+      player.pause();
+    }, 1000);
+  });
+
+  it('should send ended', (done) => {
+    player.addEventListener(player.Event.FIRST_PLAY, () => {
+      player.currentTime = player.duration - 1;
+    });
+    player.addEventListener(player.Event.ENDED, () => {
+      const payload = JSON.parse(sendSpy.lastCall.args[0]);
+      verifyPayloadProperties(payload.ks, payload.bookmark);
+      payload.bookmark.playerData.action.should.equal("FINISH");
+      done();
+    });
+    player.play();
+  });
+
+  it('should send media hit', (done) => {
+    player.addEventListener(player.Event.FIRST_PLAY, () => {
+      player.currentTime = player.duration - (player.duration + 1.5);
+    });
+    player.addEventListener(player.Event.TIME_UPDATE, () => {
+      const payload = JSON.parse(sendSpy.lastCall.args[0]);
+      verifyPayloadProperties(payload.ks, payload.bookmark);
+      if (player.currentTime == 1.5) {
+        payload.bookmark.playerData.action.should.equal("HIT");
+      }
+      done();
+    });
+    player.play();
+  });
+});
