46484112907bd142f7c51cef67480adbc27ba9ba
OrenMe <oren.me@gmail.com>
2019-06-03 21:17:41 +0300

    build: use bookmark service from NPM package

--- src/ott-analytics.js
+++ src/ott-analytics.js
@@ -1,6 +1,6 @@
 //@flow
 import {BasePlugin, Error, FakeEvent} from 'playkit-js';
-import {OTTBookmarkService, RequestBuilder} from 'playkit-js-providers/dist/playkit-bookmark-service';
+import {OTTBookmarkService, RequestBuilder} from '@playkit-js/ott-provider/dist/services/bookmark';
 
 type OttAnalyticsEventType = {[event: string]: string};
 const MEDIA_TYPE = 'MEDIA';
